64fe13538d43f7e2601f95bf5230e23c21182ec1
ma2ge <takayuki.1229@gmail.com>
2012-07-07 15:10:08 +0900

    create tag update pages

--- Handler/Tag.hs
+++ Handler/Tag.hs
@@ -5,14 +5,14 @@ import Import
 
 getTagCreateR :: Handler RepHtml
 getTagCreateR = do
-    (formWidget, formEnctype) <- generateFormPost tagForm
+    (formWidget, formEnctype) <- generateFormPost (tagForm Nothing)
     defaultLayout $ do
         setTitle "TagCreate"
         $(widgetFile "tagform")
 
 postTagCreateR :: Handler RepHtml
 postTagCreateR = do
-    ((result, formWidget), formEnctype) <- runFormPost tagForm
+    ((result, formWidget), formEnctype) <- runFormPost (tagForm Nothing)
     case result of
         FormSuccess res -> do
             _ <- runDB $ insert res
@@ -23,8 +23,28 @@ postTagCreateR = do
         setTitle "TagCreate"
         $(widgetFile "tagform")
 
-tagForm :: Form Tag
-tagForm = renderDivs $ Tag
-    <$> areq textField "Name" Nothing
+tagForm :: Maybe Tag -> Form Tag
+tagForm mtag = renderDivs $ Tag
+    <$> areq textField "Name" (fmap tagName mtag)
 
+getTagUpdateR :: TagId -> Handler RepHtml
+getTagUpdateR tagid = do
+    tag <- runDB $ get404 tagid
+    (formWidget, formEnctype) <- generateFormPost (tagForm $ Just tag)
+    defaultLayout $ do
+        setTitle "TagUpdate"
+        $(widgetFile "tagform")
+
+postTagUpdateR :: TagId -> Handler RepHtml
+postTagUpdateR tagid = do
+    ((result, formWidget), formEnctype) <- runFormPost (tagForm Nothing)
+    case result of
+        FormSuccess res -> do
+            _ <- runDB $ update tagid [TagName =. tagName res]
+            return ()
+        _ -> do
+            return ()
+    defaultLayout $ do
+        setTitle "TagUpdate"
+        $(widgetFile "tagform")
 
